7bae012f367cbf91a33e9db6e6aa7bcacbd1cb44
fabiano.lopes.viana <fabianolopesviana@gmail.com>
2020-09-03 19:28:00 -0300

    Alterado app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     //origin: 'http://localhost:3000',
-    origin: 'https://desafio-mod-4-grades-app.herokuapp.com/',
+    origin: 'https://desafio-mod-4-grades-app.herokuapp.com:59456/',
   })
 );
 
