954bfd35a148b7a7127af520d1effd64b7d9d2af
fabiano.lopes.viana <fabianolopesviana@gmail.com>
2020-09-02 22:15:24 -0300

    Commit Inicial

---
+++ models/index.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+import gradesModel from './gradesModel.js';
+
+const db = {};
+db.mongoose = mongoose;
+db.url = process.env.MONGODB;
+db.grade = gradesModel(mongoose);
+
+export { db };
